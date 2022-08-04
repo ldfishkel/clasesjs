7251834be5fde0781a7a733139889cdae0e94a9a
ldfishkel <ldfishkel@gmail.com>
2022-08-04 20:10:08 -0300

    ejercicio terminado, faltan los comentarios de comprension

--- miPrimerScript.js
+++ miPrimerScript.js
@@ -1,13 +1,29 @@
-var miInput = document.getElementById("miInput");
+var miInput = document.getElementById("miInput"); // estoy asignando un elemento html a una variable global 
 var miBoton = document.getElementById("boton"); 
-let miLista = document.getElementById("miLista");
+var miLista = document.getElementById("miLista");
 
 //esta es una variable de tipo ARRAY (lista en javascript) donde vamos a guardar los datos ingresados
-var listaDeDatos = []; 
+var listaDeDatos = []; // estoy asignando una lista vacia a una variable
+
 
 //////////////////////////////////////////////////////////////////////
 // declaro mis funciones
 //////////////////////////////////////////////////////////////////////
+
+var mostrarElemento = (idElementoAMostrar) => { //es una funcion para mostrar la lista con el id del elemento
+    let elemento = document.getElementById(idElementoAMostrar) //asigno a una variable un elemento html por id
+
+    if (elemento) // aca estoy validando si el elemento es distinto de null
+        elemento.classList.remove("oculto"); // le saco la clase "oculto" al elemento que se encuentra en la variable elemento
+}
+
+var ocultarElemento = (idElementoAOcultar) => {
+    let elemento = document.getElementById(idElementoAOcultar)
+
+    if (elemento)
+        elemento.classList.add("oculto");
+}
+
 var obtenerTexto = () => miInput.value;
 
 // esta funcion devuelve true si el texto existe en la lista de datos
@@ -35,6 +51,15 @@ var agregarALaLista = (texto) => {
     devolverElFocoAlInput();
 }
 
+const checkeoDeCantidadDeItems = () => {
+    if (listaDeDatos.length > 0) { //esta es la forma de ver si un array no esta vacio. estamos preguntando si su longitud (length) es mayor a 0
+        ocultarElemento("mensajeError");
+        mostrarElemento("miLista");
+    } else {
+        ocultarElemento("miLista");
+        mostrarElemento("mensajeError");
+    }
+}
 
 //////////////////////////////////////////////////////////////////////
 // este es el evento del click
@@ -46,9 +71,12 @@ miBoton.onclick = () => {
         mostrarMensajeError(textoEscrito); // esto se ejecuta en caso de que se cumpla la condicion
     else
         agregarALaLista(textoEscrito); // esto se ejecuta en caso de que NO se cumpla la condicion
+
+    checkeoDeCantidadDeItems();
 }
 
 miLista.onclick = (event) => {
     listaDeDatos = listaDeDatos.filter(item => item != event.target.innerText)
     event.target.remove(); 
+    checkeoDeCantidadDeItems();
 }
